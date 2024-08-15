ea2178d02b91904adf2dfe3ed53b3f65db122873
NguyenDatGH <tandatbtqb@gmail.com>
2024-08-16 01:39:47 +0700

    fix some err

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -10,7 +10,7 @@ export class OpenaiService {
   async createChatCompletion(messages: ChatCompletionMessageDto[]) {
     return this.openai.chat.completions.create({
       messages: messages as ChatCompletionMessageParam[],
-      model: 'gpt-4',
+      model: 'gpt-3.5-turbo-16k-0613',
     });
   }
 }
